15ac6c87c902704d0ea0f20c663d9bc2977267f7
Akasha <81232315+AkashaRojee@users.noreply.github.com>
2021-06-27 17:36:55 +0400

    Add utterances JS script file

---
+++ assets/js/utterances.js
@@ -0,0 +1,8 @@
+<script src="https://utteranc.es/client.js"
+        repo="AkashaRojee/Akasha.Rojee.github.io"
+        issue-term="title"
+        label="comment thread"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+</script>
